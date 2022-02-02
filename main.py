f1eb1bc0a1e7beb0720185bf51bb59d8a105212b
emailmeydrive <emailme.ydrive@gmail.com>
2022-02-02 14:54:39 +0200

    קול פיצוץ

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
+from tkinter import font
 import pygame
 
 # screen size 
 WINDOW_W = 1000
 WINDOW_H = 664
 WINDOW_SIZE = (WINDOW_W, WINDOW_H)
-
+score = 0
 
 
 pygame.init()
@@ -31,13 +32,27 @@ x_step = 10
 laser_list = []
 play = True
 
+def is_laser_hit(laser_pos):
+  return abs(laser_pos[0]-circle_x) <50 and abs(laser_pos[1]-circle_y) <50 
+
+
+EXPLOSION = "explosion sound.mp3"
 
 # prints all the laser on the screen
 def print_lasers():
+  global score
+  global circle_x
   for i in range(len(laser_list)):
-    l = laser_list[i]
-    screen.blit(laser_image,(l[0],l[1]))
-    laser_list[i] = [l[0],l[1]-30]
+    lazer = laser_list[i]
+    screen.blit(laser_image,(lazer[0],lazer[1]))
+    laser_list[i] = [lazer[0],lazer[1]-30]
+    if is_laser_hit(lazer):
+      pygame.mixer.init()
+      pygame.mixer.music.load(EXPLOSION)
+      pygame.mixer.Channel(2).play(pygame.mixer.Sound(EXPLOSION))
+      score += 1
+      circle_x = 0
+
 
   if len(laser_list) > 0 and laser_list[0][1] < 0:
     laser_list.remove(laser_list[0])
@@ -49,9 +64,14 @@ pygame.mixer.music.load(SOUND_FILE)
 pygame.mixer.music.load(GUN_SHOOT)
 pygame.mixer.Channel(0).play(pygame.mixer.Sound(SOUND_FILE))
 
+BLACK = (0, 0, 0)
+font = pygame.font.SysFont(None, 50)
 
 while play:
   screen.blit(bk_image,(0,0))
+  img = font.render('score:' + str(score), True, BLACK)
+  screen.blit(img, (60,40 ))
+
   for event in pygame.event.get():
     if event.type == pygame.QUIT:
       play = False
@@ -65,13 +85,8 @@ while play:
         pygame.mixer.Channel(1).play(pygame.mixer.Sound(GUN_SHOOT))
 
 
- 
-
-       
-
-
   screen.blit(ship_image,(ship_x,ship_y))
-  pygame.draw.circle(screen,(0,0,0),(circle_x , circle_y),10)
+  pygame.draw.circle(screen,(0,0,0),(circle_x , circle_y),20)
   print_lasers()
 
   circle_x +=circle_x_step
